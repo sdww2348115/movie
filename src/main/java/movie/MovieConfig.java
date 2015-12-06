005692370d2f6caa8e3f3808a54d405398b0051a
wangxuan <2324717228@qq.com>
2015-12-06 23:33:06 +0800

    经过使用Java VisualVM查明该程序为正常退出，继续研究AOP

--- src/main/java/movie/MovieConfig.java
+++ src/main/java/movie/MovieConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.scheduler.QueueScheduler;
 
 /**
  * 工程的自定义Bean
@@ -20,5 +21,14 @@ public class MovieConfig {
     @Bean
     public Spider movieSpider() {
         return Spider.create(new DoubanMoviePageProcessorB()).addUrl("http://movie.douban.com/tag/").addPipeline(moviePipeline).thread(5);
+    }
+
+    /**
+     * 由于spring的AOP只支持接口继承，创建一个queueScheduler来打印相应日志，查看queueScheduler中的url个数
+     * @return
+     */
+    @Bean
+    public QueueScheduler queueScheduler() {
+        return new QueueScheduler();
     }
 }
--- src/main/java/movie/framework/aspect/SchedulerAspect.java
+++ src/main/java/movie/framework/aspect/SchedulerAspect.java
@@ -1,7 +1,8 @@
 package movie.framework.aspect;
 
 import org.aspectj.lang.JoinPoint;
-import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Task;
 import us.codecraft.webmagic.scheduler.QueueScheduler;
 
 /**
@@ -12,8 +13,35 @@ public class SchedulerAspect {
     public static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(SchedulerAspect.class);
 
     public void before(JoinPoint joinPoint) {
-        Spider spider = (Spider)joinPoint.getTarget();
-        QueueScheduler scheduler = (QueueScheduler)spider.getScheduler();
-        logger.info("Alive threads : " + spider.getThreadAlive() + "\n" + "Left Requests Count: " + scheduler.getLeftRequestsCount(spider) + " Total Requests Count: " + scheduler.getTotalRequestsCount(spider));
+
+        try {
+
+            QueueScheduler scheduler = (QueueScheduler) joinPoint.getTarget();
+
+            //由源码可知，整个scheduler只有这么一个单例，task参数在此无用，所以给一个默认参数
+            Task task = new Task() {
+                @Override
+                public String getUUID() {
+                    return null;
+                }
+
+                @Override
+                public Site getSite() {
+                    return null;
+                }
+            };
+
+            //找出task参数
+            Object[] args = joinPoint.getArgs();
+            for (Object arg : args) {
+                if (Task.class.isInstance(arg)) {
+                    task = (Task) arg;
+                }
+            }
+
+            logger.info("Alive threads : " + "Left Requests Count: " + scheduler.getLeftRequestsCount(task) + " Total Requests Count: " + scheduler.getTotalRequestsCount(task));
+        } catch (Exception e) {
+            logger.warn("SchedulerAspect before error!");
+        }
     }
 }
