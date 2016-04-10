74611a0f4d94b04591d0f3b0b3f9cf9d5336b44b
sdww <2324717228@qq.com>
2016-04-10 20:14:48 +0800

    wu

---
+++ src/main/java/movie/entity/UrlProperties.java
@@ -0,0 +1,17 @@
+package movie.entity;
+
+/**
+ * Created by sdww on 2016/4/10.
+ */
+public class UrlProperties {
+
+    private String baseUrl;
+
+    public String getBaseUrl() {
+        return baseUrl;
+    }
+
+    public void setBaseUrl(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+}
